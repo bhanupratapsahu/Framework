ac4481450bdba9fc75ad76c7daa67ebaef05b292
Bhanu Pratap Sahu <bhanu@ATMECSINDT-072.(none)>
2014-08-22 14:56:56 +0530

    Previous state

--- src/main/java/com/atmecs/controllers/RequestCreator.java
+++ src/main/java/com/atmecs/controllers/RequestCreator.java
@@ -1,20 +1,26 @@
 package com.atmecs.controllers;
 
+import com.atmecs.rest_request.DELETERequest;
+import com.atmecs.rest_request.GETRequest;
+import com.atmecs.rest_request.POSTRequest;
+import com.atmecs.rest_request.PUTRequest;
+import com.jayway.restassured.path.json.JsonPath;
+
 public class RequestCreator {
 	
-	public GET createGETRequest(JSONObject jsonObject) {
+	public GETRequest createGETRequest(JsonPath jsonPath) {
 		return null;
 	}
 	
-	public PUT createPUTRequest(JSONObject jsonObject) {
+	public PUTRequest createPUTRequest(JsonPath jsonPath) {
 		return null;
 	}
 	
-	public POST createPOSTRequest(JSONObject jsonObject) {
+	public POSTRequest createPOSTRequest(JsonPath jsonPath) {
 		return null;
 	}
 	
-	public DELETE createDELETERequest(JSONObject jsonObject) {
+	public DELETERequest createDELETERequest(JsonPath jsonPath) {
 		return null;
 	}
 
--- src/main/java/com/atmecs/controllers/RestController.java
+++ src/main/java/com/atmecs/controllers/RestController.java
@@ -3,23 +3,5 @@ package com.atmecs.controllers;
 
 public class RestController {
 
-	public Request requestReference() {
-		return null;
-	}
 	
-	public RequestCreator requestCreatorObject() {
-		return null;
-	}
-	
-	public JsonParser parseFile() {
-		return null;
-	}
-	
-	public Response responseRef() {
-		return null;
-	}
-	
-	public Assertion assertionObject() {
-		return null;
-	}
 }
